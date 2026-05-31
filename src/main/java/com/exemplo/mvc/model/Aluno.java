f32a2a632a3f0de3d7f7fe49b80ec711e0aa937d
GiuliaMattedi <giuliamattedi22@gmail.com>
2026-05-31 13:03:56 -0300

    mvc-web-alunos - projeto AS

---
+++ src/main/java/com/exemplo/mvc/model/Aluno.java
@@ -0,0 +1,25 @@
+package com.exemplo.mvc.model;
+
+public class Aluno {
+
+    private String nome;
+    private String matricula;
+
+    public Aluno(String nome, String matricula) {
+
+        if (nome == null || nome.isBlank()) {
+            throw new IllegalArgumentException("Nome obrigatório");
+        }
+
+        this.nome = nome;
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+}
